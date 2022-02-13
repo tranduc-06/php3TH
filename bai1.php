9f60bfa18c2f3da21baac79f9d7f2c8d72c0e273
ductran-06 <tranmanhduc0964875742@gmail.com>
2022-02-13 20:02:27 +0700

    convert PHP

--- bai1.php
+++ bai1.php
@@ -1,3 +1,36 @@
+<form method="POST">
+<textarea name="content">
 <?php
-echo "hello world";
+    if (isset($_POST['content'])) {
+        echo $_POST['content'];
+    }
 ?>
+</textarea>
+<input type="submit" name="Convert">
+</form>
+
+<textarea>
+<?php
+    function convertPhp($input) {
+        $a = strpos($input,'$');
+        $b = strpos($input,'_');
+        $char = substr($input, $a, 2);
+        $char1 = substr($input, $b, 2);
+        $low = str_replace($char, strtolower($char), $input);
+        $low1 = str_replace($char1, strtolower($char1), $low);
+        
+        $c = str_replace( '=', ' = ', $low1);
+        $d = str_replace( '_', '', $c );
+
+        $BehindEqual = str_replace( 'array(', '[', $d);
+        $BehindEqual1 = str_replace( ')', ']', $BehindEqual);
+
+
+        return $BehindEqual1;
+    }
+
+    if (isset($_POST['content'])) {
+        echo convertPhp($_POST['content']);
+    }
+?>
+</textarea>
---
+++ hello.php
@@ -0,0 +1,5 @@
+<textarea name="" id="" cols="30" rows="10">
+<?php
+    echo $_POST["cv"]."ccc";
+?>
+</textarea>
